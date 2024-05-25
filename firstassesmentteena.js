b269117a0917d04144836ff04e5cf5be5fb6ef27
Teena Neupane <127821631+Tea-nah2003@users.noreply.github.com>
2024-05-25 11:49:45 +0530

    Add files via upload

---
+++ firstassesmentteena.js
@@ -0,0 +1,51 @@
+/*let nft will store metadata, there are many entries 
+it will be stored in an array*/
+let NFTs=[];
+//initially the id will be assigned as 1
+let _tokenid=1;    
+//token_id is declared outdise local scope i.e global scope
+function mintNFT(Name,Region,Color)
+{
+    const metadata=
+    {
+        "Tokenid":_tokenid,
+        "Name":Name,
+        "Region":Region,
+        "Color":Color
+    };
+    //putting the metadata into the nft
+    NFTs.push(metadata);
+    //adding the values of the token
+    _tokenid++;
+}
+function listNFTS()
+{
+    let i=0;
+    while(i<NFTs.length)
+    {
+        console.log("Uniquetokenid=",NFTs[i].Tokenid);
+        console.log("Name=",NFTs[i].Name);
+        console.log("Region=",NFTs[i].Region);
+        console.log("Color=",NFTs[i].Color);
+        console.log("");
+        i++;
+    }
+}
+// print the total number of NFTs we have minted to the console
+function getTotalSupply()
+{
+    return NFTs.length;
+}
+//calling the functions
+
+//function 1 where metadata is stored
+mintNFT("Teena","Asia","Rainbow Splash");
+mintNFT("Harry","Europe","Skycolor");
+mintNFT("Farhat","Africa","crystal Clear");
+
+//function 2 whare we list all the values
+console.log("List of NFTs");
+listNFTS();
+
+//function 3 to print total no of nfts
+console.log("Total no of NFTs=",getTotalSupply());
